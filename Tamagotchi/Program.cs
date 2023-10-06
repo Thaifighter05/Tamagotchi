6ba216e417ccb2f8ff9c0fbd129ff79f1e5b5d33
Maximilian Persson <Maximilian.persson2@elev.ga.ntig.se>
2023-10-06 11:05:26 +0200

    1st commit

---
+++ Tamagotchi/Program.cs
@@ -0,0 +1,19 @@
+﻿using Namespace;
+
+Console.WriteLine("Hi i'm Tamagotchi");
+
+Tamagotchi Tama = new Tamagotchi();
+
+Console.WriteLine("What's your tamagotchi name");
+Tama.name = Console.ReadLine();
+
+Console.WriteLine($"{Tama.name} Is a very cute name ");
+
+// while (Tama.GetAlive()==true)
+// {
+// Tama.printstats
+// }
+
+
+
+Console.ReadLine();
---
+++ Tamagotchi/Tamagotchi.cs
@@ -0,0 +1,51 @@
+namespace Namespace;
+public class Tamagotchi
+{
+    private int hunger;
+    private int boredom;
+    private List<string> words;
+
+    private bool isAlive;
+
+    private Random generator;
+
+    public string name;
+
+    public Tamagotchi()
+    {
+
+    }
+
+    public void Tick()
+    {
+
+    }
+
+    public void Feed()
+    {
+
+    }
+
+    public void Hi()
+    {
+
+    }
+    public void Teach(string word)
+    {
+        Console.WriteLine();
+    }
+
+    public void printstats()
+    {
+        Console.WriteLine ("$ Name:{name} Boredom: {Boredom} Hunger: {Hunger}  ");
+    }
+    public bool GetAlive()
+    {
+        return true;
+    }
+    private void ReduceBoredom()
+    {
+
+    }
+
+}
